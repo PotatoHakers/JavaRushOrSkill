40d9fcb73cd90f63f47da8f13c0ab7db478aac28
unknown <s.herza@mail.ru>
2023-01-16 00:17:46 +0300

    commit

---
+++ src/Main.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+//        System.out.println("Hello World!\nHello World!");
+//
+//        byte num = 8;
+//        System.out.print(num+"\n");
+//
+//        Scanner scanner = new Scanner(System.in);
+//        Double numwithdot = scanner.nextDouble();
+//        System.out.print(numwithdot);
+
+//        float number=4.35f;
+//
+//        System.out.printf("Переменная:num=%f; \n", number);
+
+//        var num = 20;
+//        num = 5+num;
+//        System.out.print(num+"\n");
+//        System.out.printf("Переменная:var=%d", num);
+
+//        double num1 = 1.4234;
+//        System.out.println(Math.pow(num1,3));
+
+//        String[] simAr = new String[] {"J", "a", "v", "a", "!"};
+//        System.out.printf("Элемент: "+ simAr[2]);
+
+//        for (int i=1; i<6; i++)
+//        {
+//            System.out.print("Номер элемента:" + i  + "\n");
+//        }
+//        Scanner scanner = new Scanner(System.in);
+//        int num = scanner.nextInt();
+//
+//            switch (num) {
+//                case (3):
+//                    System.out.println("Три");
+//                    break;
+//                case (7):
+//                    System.out.println("Семь");
+//                    break;
+//                default:
+//                    System.out.println("Что-то другое");
+//                    break;
+//
+//            }
+//
+//        for (int i =10; i<=20;i++)
+//        {
+//            System.out.println(Math.pow(i,2));
+//        }
+//
+//        int[] array = new int[10];
+//        for (int i = 0; i < array.length; i++) {
+//            array[i] = ((int)(Math.random() * 10));
+//            System.out.println(array[i]);
+//        }
+    }
+}
